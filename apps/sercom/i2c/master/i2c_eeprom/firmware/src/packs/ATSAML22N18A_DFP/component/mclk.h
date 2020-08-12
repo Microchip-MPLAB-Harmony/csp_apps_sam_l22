987aa3adf9b7840bef4bb504b48c7f7a95e2c0a6
Prachi Phadke <prachi.phadke@microchip.com>
2020-08-13 00:14:24 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_l22_xpro
    MH3-41463
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/packs/ATSAML22N18A_DFP/component/mclk.h
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/packs/ATSAML22N18A_DFP/component/mclk.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2019-11-24T16:22:29Z */
+/* file generated from device description version 2020-08-06T15:10:46Z */
 #ifndef _SAML22_MCLK_COMPONENT_H_
 #define _SAML22_MCLK_COMPONENT_H_
 
