28fd5f08001c07518e2d996bf8533ca08ce23684
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-20 13:48:55 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_write_read_test_app_sam_l22_xpro
    MH3-41473
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/spi/master/spi_write_read_test_app/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.h
+++ apps/sercom/spi/master/spi_write_read_test_app/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.h
@@ -65,45 +65,41 @@
 // *****************************************************************************
 // *****************************************************************************
 
-  
 /*** Macros for APP_SLAVE_BUSY_PIN pin ***/
 #define APP_SLAVE_BUSY_PIN_Set()               (PORT_REGS->GROUP[2].PORT_OUTSET = 1 << 0)
 #define APP_SLAVE_BUSY_PIN_Clear()             (PORT_REGS->GROUP[2].PORT_OUTCLR = 1 << 0)
 #define APP_SLAVE_BUSY_PIN_Toggle()            (PORT_REGS->GROUP[2].PORT_OUTTGL = 1 << 0)
-#define APP_SLAVE_BUSY_PIN_Get()               (((PORT_REGS->GROUP[2].PORT_IN >> 0)) & 0x01)
 #define APP_SLAVE_BUSY_PIN_OutputEnable()      (PORT_REGS->GROUP[2].PORT_DIRSET = 1 << 0)
 #define APP_SLAVE_BUSY_PIN_InputEnable()       (PORT_REGS->GROUP[2].PORT_DIRCLR = 1 << 0)
+#define APP_SLAVE_BUSY_PIN_Get()               (((PORT_REGS->GROUP[2].PORT_IN >> 0)) & 0x01)
 #define APP_SLAVE_BUSY_PIN_PIN                  PORT_PIN_PC00
 
 /*** Macros for SWITCH pin ***/
 #define SWITCH_Set()               (PORT_REGS->GROUP[2].PORT_OUTSET = 1 << 1)
 #define SWITCH_Clear()             (PORT_REGS->GROUP[2].PORT_OUTCLR = 1 << 1)
 #define SWITCH_Toggle()            (PORT_REGS->GROUP[2].PORT_OUTTGL = 1 << 1)
-#define SWITCH_Get()               (((PORT_REGS->GROUP[2].PORT_IN >> 1)) & 0x01)
 #define SWITCH_OutputEnable()      (PORT_REGS->GROUP[2].PORT_DIRSET = 1 << 1)
 #define SWITCH_InputEnable()       (PORT_REGS->GROUP[2].PORT_DIRCLR = 1 << 1)
+#define SWITCH_Get()               (((PORT_REGS->GROUP[2].PORT_IN >> 1)) & 0x01)
 #define SWITCH_PIN                  PORT_PIN_PC01
 
 /*** Macros for APP_SLAVE_CS pin ***/
 #define APP_SLAVE_CS_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 17)
 #define APP_SLAVE_CS_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 17)
 #define APP_SLAVE_CS_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 17)
-#define APP_SLAVE_CS_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 17)) & 0x01)
 #define APP_SLAVE_CS_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 17)
 #define APP_SLAVE_CS_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 17)
+#define APP_SLAVE_CS_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 17)) & 0x01)
 #define APP_SLAVE_CS_PIN                  PORT_PIN_PA17
 
 /*** Macros for LED pin ***/
 #define LED_Set()               (PORT_REGS->GROUP[2].PORT_OUTSET = 1 << 27)
 #define LED_Clear()             (PORT_REGS->GROUP[2].PORT_OUTCLR = 1 << 27)
 #define LED_Toggle()            (PORT_REGS->GROUP[2].PORT_OUTTGL = 1 << 27)
-#define LED_Get()               (((PORT_REGS->GROUP[2].PORT_IN >> 27)) & 0x01)
 #define LED_OutputEnable()      (PORT_REGS->GROUP[2].PORT_DIRSET = 1 << 27)
 #define LED_InputEnable()       (PORT_REGS->GROUP[2].PORT_DIRCLR = 1 << 27)
+#define LED_Get()               (((PORT_REGS->GROUP[2].PORT_IN >> 27)) & 0x01)
 #define LED_PIN                  PORT_PIN_PC27
-
-
-
 // *****************************************************************************
 /* PORT Group
 
@@ -1006,7 +1002,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -1045,7 +1041,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
