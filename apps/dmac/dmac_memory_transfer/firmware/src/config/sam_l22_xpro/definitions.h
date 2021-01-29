a96ae71298b73f4fee35a1a3d640ab4394139e28
Prachi Phadke <prachi.phadke@microchip.com>
2021-01-29 23:04:58 +0530

    [DMAC] Regenerated demo for dmac_dmac_memory_transfer_sam_l22_xpro
    MH3-49550
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/dmac/dmac_memory_transfer/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/dmac/dmac_memory_transfer/firmware/src/config/sam_l22_xpro/definitions.h
@@ -51,12 +51,12 @@
 #include <stdio.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/dmac/plib_dmac.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom4_usart.h"
 
 // DOM-IGNORE-BEGIN
@@ -66,6 +66,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 32000000
 
 // *****************************************************************************
 // *****************************************************************************
