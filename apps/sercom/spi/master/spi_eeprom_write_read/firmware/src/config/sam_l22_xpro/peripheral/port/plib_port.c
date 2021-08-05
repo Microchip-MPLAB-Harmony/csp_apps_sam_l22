4292af4ecdd17c4674ef6debdbaf589b357ba4c7
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-05 14:33:25 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_eeprom_write_read_sam_l22_xpro
    MH3-59064
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
@@ -75,6 +75,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_DIR = 0x20000;
    PORT_REGS->GROUP[0].PORT_OUT = 0x20000;
    PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x0;
    PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1;
 
@@ -84,10 +85,13 @@ void PORT_Initialize(void)
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x10;
    PORT_REGS->GROUP[1].PORT_OUT = 0x10;
+   PORT_REGS->GROUP[1].PORT_PINCFG[4] = 0x0;
 
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x18000000;
+   PORT_REGS->GROUP[2].PORT_PINCFG[27] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PINCFG[28] = 0x0;
 
 
 }
@@ -301,7 +305,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -312,7 +316,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
