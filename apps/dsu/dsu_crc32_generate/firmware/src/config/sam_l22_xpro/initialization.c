a03760add6e220bcd7093af3ca5c77b7bfe0e36f
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-05 14:19:40 +0530

    [DSU] Regenerated demo for dsu_dsu_crc32_generate_sam_l22_xpro
    MH3-59051
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/dsu/dsu_crc32_generate/firmware/src/config/sam_l22_xpro/initialization.c
+++ apps/dsu/dsu_crc32_generate/firmware/src/config/sam_l22_xpro/initialization.c
@@ -119,7 +119,7 @@
 void SYS_Initialize ( void* data )
 {
 
-    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3UL);
 
     PM_Initialize();
 
