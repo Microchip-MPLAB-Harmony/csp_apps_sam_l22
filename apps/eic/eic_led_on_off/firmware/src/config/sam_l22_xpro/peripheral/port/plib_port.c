eeab52aec4b4f904f7f773410b64c1d9a63e464f
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-05-19 14:17:28 +0530

    [EIC] Regenerated demo for eic_eic_led_on_off_sam_l22_xpro
    MH3-68237
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/eic/eic_led_on_off/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
+++ apps/eic/eic_led_on_off/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -83,9 +82,11 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[2].PORT_PINCFG[1] = 0x5;
    PORT_REGS->GROUP[2].PORT_PINCFG[27] = 0x0;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[0] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[13] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
