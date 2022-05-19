cf54260c57f576b515776a738d9bd19542b7117f
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-05-19 14:33:26 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_write_read_test_app_sam_l22_xpro
    MH3-68253
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/sercom/spi/master/spi_write_read_test_app/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/master/spi_write_read_test_app/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -92,9 +91,11 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[2].PORT_PINCFG[1] = 0x6;
    PORT_REGS->GROUP[2].PORT_PINCFG[27] = 0x0;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[0] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[13] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
