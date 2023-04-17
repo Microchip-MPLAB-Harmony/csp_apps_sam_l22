b10c4805e59f0b94076b2fa0697113c2391a30ab
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 15:59:21 +0530

    [PM] Regenerated demo for pm_pm_wakeup_eic_sam_l22_xpro
    MH3-79720

--- apps/pm/pm_wakeup_eic/firmware/src/config/sam_l22_xpro/peripheral/eic/plib_eic.h
+++ apps/pm/pm_wakeup_eic/firmware/src/config/sam_l22_xpro/peripheral/eic/plib_eic.h
@@ -96,14 +96,13 @@
     None.
 */
 
-typedef enum
-{
+
     /* External Interrupt Controller Pin 9 */
-    EIC_PIN_9 = 9,
+#define    EIC_PIN_9   (9U)
 
-    EIC_PIN_MAX = 16
+#define    EIC_PIN_MAX  (16U)
 
-} EIC_PIN;
+typedef uint16_t EIC_PIN;
 
 // *****************************************************************************
 /* EIC Interrupt Pin Callback Function Pointer Type
