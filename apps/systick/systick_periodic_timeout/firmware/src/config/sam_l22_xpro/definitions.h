38a6cb350e4044903e28153eaa25ad5cfe46ec46
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 15:59:47 +0530

    [SYSTICK] Regenerated demo for systick_systick_periodic_timeout_sam_l22_xpro
    MH3-79737

--- apps/systick/systick_periodic_timeout/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/systick/systick_periodic_timeout/firmware/src/config/sam_l22_xpro/definitions.h
@@ -50,11 +50,11 @@
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
+#include "peripheral/pm/plib_pm.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -63,6 +63,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML22N18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML22"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 32000000
