0f6c2302d93ce678931ec2a8639ba67d47a0f137
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 15:59:31 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_l22_xpro
    MH3-79727

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l22_xpro/definitions.h
@@ -53,8 +53,8 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/sercom/i2c_master/plib_sercom5_i2c_master.h"
 #include "peripheral/pm/plib_pm.h"
+#include "peripheral/sercom/i2c_master/plib_sercom5_i2c_master.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -63,6 +63,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML22N18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML22"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 32000000
