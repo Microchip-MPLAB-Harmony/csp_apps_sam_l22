932a6fe130face96d0c154b92e1a88fc0922c85b
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 15:59:55 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_l22_xpro
    MH3-79743

--- apps/wdt/wdt_timeout/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_l22_xpro/definitions.h
@@ -51,12 +51,12 @@
 #include <stdio.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/wdt/plib_wdt.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "peripheral/eic/plib_eic.h"
 
@@ -67,6 +67,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML22N18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML22"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 32000000
