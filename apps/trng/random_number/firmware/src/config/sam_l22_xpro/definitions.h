72452c5c601192fd9358503861fe750d847c408b
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 14:38:15 +0000

    [APP] Regenerated demo for trng__random_number__firmware
    MH3C-296

--- apps/trng/random_number/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/trng/random_number/firmware/src/config/sam_l22_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -54,8 +54,8 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/trng/plib_trng.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "peripheral/eic/plib_eic.h"
 
